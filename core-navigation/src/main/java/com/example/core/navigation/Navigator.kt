3c57aab2d6a775ddbf9d3155d76a9c8298b49c0d
wasimreza <wasim.cse.2k8@gmail.com>
2023-02-01 10:36:14 +0100

    navigation and dependencies fixed

--- core-navigation/src/main/java/com/example/core/navigation/Navigator.kt
+++ core-navigation/src/main/java/com/example/core/navigation/Navigator.kt
@@ -3,20 +3,24 @@ package com.example.core.navigation
 import androidx.navigation.NavOptionsBuilder
 import com.example.core.navigation.NavigatorEvent.Directions
 import com.example.core.navigation.NavigatorEvent.NavigateUp
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class Navigator @Inject constructor() {
-    private val navigationEvents = Channel<NavigatorEvent>(capacity = Channel.CONFLATED)
-    val destinations: Flow<NavigatorEvent> = navigationEvents.receiveAsFlow()
-    fun navigateUp(): Boolean = navigationEvents.trySend(NavigateUp).isSuccess
+    private val _destinations = MutableStateFlow<NavigatorEvent?>(null)
+
+    val destinations: StateFlow<NavigatorEvent?> = _destinations
+    fun navigateUp() {
+        _destinations.value = NavigateUp
+    }
+
     fun navigate(
         route: String,
-        builder: NavOptionsBuilder.() -> Unit = { launchSingleTop = true }
-    ): Boolean =
-        navigationEvents.trySend(Directions(route, builder)).isSuccess
+        builder: NavOptionsBuilder.() -> Unit = { launchSingleTop = true },
+    ) {
+        _destinations.value = Directions(route, builder)
+    }
 }
